1fdc624680ad58691a067776c3f87327e565f9e4
sebaek <sebaek@gmail.com>
2021-09-01 12:49:01 +0900

    car, tesla

--- src/p01review/C04ReferenceType.java
+++ src/p01review/C04ReferenceType.java
@@ -3,11 +3,16 @@ package p01review;
 public class C04ReferenceType {
 	public static void main(String[] args) {
 		Tesla t1 = new Tesla();
+		Audi a1 = new Audi();
 		Car c1 = t1;
+		c1 = a1;
 		
 		c1.setSpeed(100);
 		
 		System.out.println(t1.getSpeed());
+		
+		t1.autoControl();
+//		c1.autoControl();
 	}
 }
 
@@ -25,4 +30,20 @@ class Car {
 
 class Tesla extends Car {
 	
+	public void autoControl() {
+		System.out.println("자동 운전");
+	}
 }
+
+class Audi extends Car {
+	
+}
+
+
+
+
+
+
+
+
+
