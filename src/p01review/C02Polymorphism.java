6d2363d5c839a59a5f96fd70dba5bf85057b7fe3
sebaek <sebaek@gmail.com>
2021-09-01 12:38:13 +0900

    다형성 설명 추가

--- src/p01review/C02Polymorphism.java
+++ src/p01review/C02Polymorphism.java
@@ -2,6 +2,9 @@ package p01review;
 
 public class C02Polymorphism {
 	// 다형성
+	
+	// 1.상위 타입으로 자동 변환(값 할당)
+	// 2.실행되는 메소드는 실제 인스턴스의 메소드
 	public static void main(String[] args) {
 		Animal a1 = new Animal();
 		Cat c1 = new Cat();
