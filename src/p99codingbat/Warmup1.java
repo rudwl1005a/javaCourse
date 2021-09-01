57944de7f0d48fdf6a9eda56e3d51f29de342629
sebaek <sebaek@gmail.com>
2021-09-01 14:31:59 +0900

    sleepin

---
+++ src/p99codingbat/Warmup1.java
@@ -0,0 +1,15 @@
+package p99codingbat;
+
+public class Warmup1 {
+	public boolean sleepIn(boolean weekday, boolean vacation) {
+		/*
+		 * boolean result = !weekday || vacation;
+		 * 
+		 * return result;
+		 */
+
+		return !weekday || vacation;
+
+	}
+
+}
