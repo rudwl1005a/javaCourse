ce10a057eeb0a728acb438751e86bf1f62757c90
sebaek <sebaek@gmail.com>
2021-09-02 12:03:58 +0900

    quiz 1

---
+++ src/p07extends/p02quiz/Car.java
@@ -0,0 +1,8 @@
+package p07extends.p02quiz;
+
+public class Car {
+
+	public void start() {
+		System.out.println("차가 출발 합니다.");
+	}
+}
---
+++ src/p07extends/p02quiz/CarExample.java
@@ -0,0 +1,15 @@
+package p07extends.p02quiz;
+
+public class CarExample {
+	public static void main(String[] args) {
+		Car car;
+		
+		car = new Kia();
+		
+		car.start();
+		
+		// 콘솔 출력 결과
+		// "기아차가 출발합니다."
+		
+	}
+}
