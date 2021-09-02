1945b244cc2732ae8bc9f3b9225405924ff65b05
sebaek <sebaek@gmail.com>
2021-09-02 14:39:43 +0900

    instanceOf

---
+++ src/p07extends/p03lecture/p01casting/InstanceOfExample.java
@@ -0,0 +1,20 @@
+package p07extends.p03lecture.p01casting;
+
+public class InstanceOfExample {
+	public static void main(String[] args) {
+		Animal a1 = new Cat();
+		Animal a2 = new Dog();
+		
+		if (a1 instanceof Cat) {
+			Cat c1 = (Cat) a1;
+			c1.punch();
+		}
+
+		if (a2 instanceof Cat) {
+			Cat c2 = (Cat) a2;
+			c2.punch();
+		}
+		
+		System.out.println("프로그램 정상 종료.");
+	}
+}
