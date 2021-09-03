39c084dcc44bf36e62cfb9087203ea48cd244869
sebaek <sebaek@gmail.com>
2021-09-03 14:19:53 +0900

    int max, min

--- src/p06class/p02quiz/p01static/Quiz02.java
+++ src/p06class/p02quiz/p01static/Quiz02.java
@@ -7,6 +7,10 @@ public class Quiz02 {
 		//                            최소값
 		// 을 api에서 Integer 클래스를 찾아서
 		// 관련된 필드를 사용해서 출력해보세요.
+		System.out.println("최대값:" + Integer.MAX_VALUE);
+		System.out.println("최소값:" + Integer.MIN_VALUE);
+		
+		// 정수 (long) 최대값, 최소값
 		
 	}
 }
