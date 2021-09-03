41db62c66acf5ce66d9d101ebedf64383fbecd23
sebaek <sebaek@gmail.com>
2021-09-03 10:35:32 +0900

    abstract method, class

--- src/p07extends/p03lecture/p03abstract_method/Animal.java
+++ src/p07extends/p03lecture/p03abstract_method/Animal.java
@@ -1,5 +1,9 @@
 package p07extends.p03lecture.p03abstract_method;
 
-public class Animal {
+public abstract class Animal {
+	// 추상 메소드가 있는 클래스는 반드시 추상 클래스이어야함.
 
+	public abstract void cry(); // 추상 메소드 (몸통없는 메소드)
+	
+	
 }
--- src/p07extends/p03lecture/p03abstract_method/Dog.java
+++ src/p07extends/p03lecture/p03abstract_method/Dog.java
@@ -1,5 +1,8 @@
 package p07extends.p03lecture.p03abstract_method;
 
-public class Dog {
+public class Dog extends Animal {
 
+	public void cry() {
+		System.out.println("멍멍");
+	}
 }
