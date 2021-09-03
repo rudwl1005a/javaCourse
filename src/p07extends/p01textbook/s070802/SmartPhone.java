2651f4140c45f5b096ca752a1f1fe222eb4d9f12
sebaek <sebaek@gmail.com>
2021-09-03 10:14:56 +0900

    overrice turnon

--- src/p07extends/p01textbook/s070802/SmartPhone.java
+++ src/p07extends/p01textbook/s070802/SmartPhone.java
@@ -5,4 +5,9 @@ public class SmartPhone extends Phone {
 	public void internetSearch() {
 		System.out.println("인터넷 검색을 합니다.");
 	}
+	
+	@Override
+	public void turnOn() {
+		System.out.println("스마트폰이 켜집니다.");
+	}
 }
--- src/p07extends/p01textbook/s070802/TelePhone.java
+++ src/p07extends/p01textbook/s070802/TelePhone.java
@@ -4,4 +4,9 @@ public class TelePhone extends Phone {
 	public void autoAnswering() {
 		System.out.println("자동응답합니다.");
 	}
+	
+	@Override
+	public void turnOn() {
+		System.out.println("자동응답전화기가 켜집니다.");
+	}
 }
