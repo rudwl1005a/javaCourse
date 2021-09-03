b76a15094f6d2ee1b19fc53fa7f0477524844c15
sebaek <sebaek@gmail.com>
2021-09-03 12:13:48 +0900

    final class

---
+++ src/p07extends/p03lecture/p04final/FinalEx04Class.java
@@ -0,0 +1,18 @@
+package p07extends.p03lecture.p04final;
+
+public class FinalEx04Class {
+	public static void main(String[] args) {
+		int j = 0;
+		
+		System.out.println(j);
+	}
+}
+
+final class TV {
+	//상속할 수 없는 클래스
+	
+}
+
+class SmartTV extends TV {
+	
+}
