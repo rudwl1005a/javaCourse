6954ac206eb2f5aaf0c68f5a42d726c9fc1bf3f0
sebaek <sebaek@gmail.com>
2021-09-03 10:07:57 +0900

    exmaple

--- src/p07extends/p01textbook/s070802/Example.java
+++ src/p07extends/p01textbook/s070802/Example.java
@@ -1,5 +1,16 @@
 package p07extends.p01textbook.s070802;
 
 public class Example {
-
+	public static void main(String[] args) {
+//		Phone p1 = new Phone();
+		Phone p2 = new TelePhone();
+		Phone p3 = new SmartPhone();
+		
+		p2.turnOn();
+//		p2.autoAnswering();
+		
+		p3.turnOn();
+//		p3.internetSearch();
+		
+	}
 }
