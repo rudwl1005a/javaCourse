3e5eabb24e870f61b7d40a8004b9aab5d2eaab3d
sebaek <sebaek@gmail.com>
2021-09-03 10:08:59 +0900

    Update SmartPhone.java

--- src/p07extends/p01textbook/s070802/SmartPhone.java
+++ src/p07extends/p01textbook/s070802/SmartPhone.java
@@ -2,7 +2,7 @@ package p07extends.p01textbook.s070802;
 
 public class SmartPhone extends Phone {
 
-	public void internetSerch() {
+	public void internetSearch() {
 		System.out.println("인터넷 검색을 합니다.");
 	}
 }
