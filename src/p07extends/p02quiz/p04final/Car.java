3ae3c96d0748ff14dccf3293ba92e0dae13d282b
sebaek <sebaek@gmail.com>
2021-09-03 12:03:42 +0900

    create car.java with fianl field

---
+++ src/p07extends/p02quiz/p04final/Car.java
@@ -0,0 +1,17 @@
+package p07extends.p02quiz.p04final;
+
+public class Car {
+	private final String model;
+	
+	public Car() {
+		model = "kia";
+	}
+	
+	public Car(String model) {
+		this.model = model;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+}
