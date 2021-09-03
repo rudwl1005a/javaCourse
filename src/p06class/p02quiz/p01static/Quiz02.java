dbb1eed81e14fb6e6f429c6fc2a900af56f0c6fd
sebaek <sebaek@gmail.com>
2021-09-03 14:12:44 +0900

    integer

--- src/p06class/p02quiz/p01static/Quiz02.java
+++ src/p06class/p02quiz/p01static/Quiz02.java
@@ -2,4 +2,11 @@ package p06class.p02quiz.p01static;
 
 public class Quiz02 {
 
+	public static void main(String[] args) {
+		// 정수(int)로 표현할 수 있는 최대값
+		//                            최소값
+		// 을 api에서 Integer 클래스를 찾아서
+		// 관련된 필드를 사용해서 출력해보세요.
+		
+	}
 }
