d93c49b6302272caf494597affeff0259ab0669c
sebaek <sebaek@gmail.com>
2021-09-06 10:27:44 +0900

    interface

--- src/p08interface/p03lecture/Malamute.java
+++ src/p08interface/p03lecture/Malamute.java
@@ -1,6 +1,6 @@
 package p08interface.p03lecture;
 
-public class Malamute extends KindaDog {
+public class Malamute extends KindaDog implements Pet {
 	@Override
 	public void bark() {
 		System.out.println("멍멍");
