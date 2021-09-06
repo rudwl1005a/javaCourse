8f0e3f301fc530ad0cc6f456ac1354079bc45b41
sebaek <sebaek@gmail.com>
2021-09-06 11:14:17 +0900

    abstract method

--- src/p08interface/p03lecture/Cat.java
+++ src/p08interface/p03lecture/Cat.java
@@ -1,5 +1,8 @@
 package p08interface.p03lecture;
 
 public class Cat extends KindaCat implements Pet {
-	
+	@Override
+	public void sit() {
+		System.out.println("무시합니다.");
+	}
 }
--- src/p08interface/p03lecture/Pet.java
+++ src/p08interface/p03lecture/Pet.java
@@ -1,5 +1,9 @@
 package p08interface.p03lecture;
 
 public interface Pet {
-
+	
+	// 인터페이스의 메소드는 추상(abstract) 메소드, 그리고 public
+//	public abstract void sit();
+//	public void sit();
+	void sit();
 }
