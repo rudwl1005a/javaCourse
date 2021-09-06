891334741880aeeb51cb2a1dac8662fa6101b7fa
sebaek <sebaek@gmail.com>
2021-09-06 15:06:02 +0900

    default method

--- src/p08interface/p01textbook/RemoteControl.java
+++ src/p08interface/p01textbook/RemoteControl.java
@@ -6,4 +6,12 @@ public interface RemoteControl {
 	public void turnOff();
 	public void setVolume(int volume);
 	
+	default void setMute(boolean mute) {
+		if (mute) {
+			System.out.println("무음 처리합니다.");
+		} else {
+			System.out.println("무음 해제합니다.");
+		}
+	}
+	
 }
