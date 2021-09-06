995fbfc010dd05093fab62d5f0290c65a2875d3e
sebaek <sebaek@gmail.com>
2021-09-06 11:04:25 +0900

    type conversion, instanceof

---
+++ src/p08interface/p01textbook/RemoteControlExample.java
@@ -0,0 +1,15 @@
+package p08interface.p01textbook;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		RemoteControl rc;
+		
+		rc = new Television();
+		rc = new Audio();
+		
+		System.out.println(rc instanceof RemoteControl);
+		System.out.println(rc instanceof Audio);
+		
+		System.out.println(rc instanceof Television);
+	}
+}
