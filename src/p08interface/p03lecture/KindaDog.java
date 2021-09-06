439a05f97185eabc447425d1ff8ed9c199fc10cb
sebaek <sebaek@gmail.com>
2021-09-06 10:27:24 +0900

    interface

---
+++ src/p08interface/p03lecture/KindaDog.java
@@ -0,0 +1,5 @@
+package p08interface.p03lecture;
+
+public abstract class KindaDog {
+	public abstract void bark();
+}
---
+++ src/p08interface/p03lecture/Malamute.java
@@ -0,0 +1,8 @@
+package p08interface.p03lecture;
+
+public class Malamute extends KindaDog {
+	@Override
+	public void bark() {
+		System.out.println("멍멍");
+	}
+}
