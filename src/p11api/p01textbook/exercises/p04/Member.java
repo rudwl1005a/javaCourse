2c6f591ffff5beda0e4296ef8397204af0dd075b
Min kyungdae <rudwl1005@naver.com>
2021-11-26 03:58:02 +0900

    toString

---
+++ src/p11api/p01textbook/exercises/p04/Member.java
@@ -0,0 +1,17 @@
+package p11api.p01textbook.exercises.p04;
+
+public class Member {
+	private String id;
+	private String name;
+	public Member(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return id + ": " + name;
+	}
+	
+}
---
+++ src/p11api/p01textbook/exercises/p04/MemberExample.java
@@ -0,0 +1,13 @@
+package p11api.p01textbook.exercises.p04;
+
+public class MemberExample {
+
+	// 568쪽
+	
+	public static void main(String[] args) {
+		Member member = new Member("blue", "이파란");
+		System.out.println(member); // blue: 이파란
+		System.out.println(member.toString());
+	
+	}
+}
